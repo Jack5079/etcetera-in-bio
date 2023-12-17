ea64f702de0d0fc97b43da278d9b0aa53ed204fa
Jack W. <git@jack.cab>
2023-12-17 17:01:20 -0500

    feat: :tada: init

---
+++ src/server.js
@@ -0,0 +1,12 @@
+// import * as Run from "@marko/run/router"
+// import { Hono } from "hono"
+
+// const server = new Hono()
+
+// server.use((ctx) => {
+// 	return Run.fetch(ctx.req.raw, {})
+// })
+
+// export default server // Wave smiley!
+
+export * as default from "@marko/run/router"
